1381b569c7ded71e8ceb86a3815bfbe7fde9afd4
Erik Hals <erikhals@gmail.com>
2021-06-14 20:21:17 +0200

    Get preloader working

--- preload.js
+++ preload.js
@@ -1,10 +1,21 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+"use strict";
+let jsonData = require("./lister.json");
 
-  for (const dependency of ["forste"]) {
-    replaceText(dependency, "hello");
+window.addEventListener("DOMContentLoaded", () => {
+  for (let u = 0; u < 6; u++) {
+    let v = u + 1;
+
+    let featureText = document.getElementById("featuretext" + v);
+    featureText.innerHTML = jsonData[u].title;
+
+    let listDiv = document.getElementById("feature" + v + "box");
+    listDiv.innerHTML = "";
+    let ul = document.createElement("ul");
+    for (var i = 0; i < jsonData[u].list.length; ++i) {
+      let li = document.createElement("li");
+      li.innerHTML = jsonData[u].list[i]; // Use innerHTML to set the text
+      ul.appendChild(li);
+    }
+    listDiv.appendChild(ul);
   }
 });
